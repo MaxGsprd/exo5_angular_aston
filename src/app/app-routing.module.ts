817d8e83dc2bc040c85e70ddfe16c1f0193efd36
MaxGsprd <max.gaspard@outlook.fr>
2020-10-30 15:24:20 +0100

    add routing for cats and form pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CatsComponent } from './pages/cats/cats.component';
+import { FormComponent } from './pages/form/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component: CatsComponent},
+  {path:"cats", component: CatsComponent},
+  {path:"form", component: FormComponent},
+  // {path:"**", component: FormComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
